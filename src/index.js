d1a734915573cef93e0672b56b3c9ea1cd680338
AmineHLub <Amino.bhl@gmail.com>
2021-09-22 00:48:46 +0100

    Fix linters

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
+// eslint-disable-next-line no-unused-vars
 import _ from 'lodash';
-import './style.css'
-//function component() {
-  //  const element = document.createElement('div');
-    //element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    //element.classList.add('hello');
-    //return element;
-  //}
-  
-  //document.body.appendChild(component());
-  //index.html
-  
+import './style.css';
+// function component() {
+//  const element = document.createElement('div');
+// element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+// element.classList.add('hello');
+// return element;
+// }
+
+// document.body.appendChild(component());
+// index.html
