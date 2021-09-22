273c8b39cec6f13b3c7c7bb77990165cf5dde777
AmineHLub <Amino.bhl@gmail.com>
2021-09-22 01:00:54 +0100

    fixing conflicts 2

--- src/index.js
+++ src/index.js
@@ -1,25 +1,12 @@
+// eslint-disable-next-line no-unused-vars
 import _ from 'lodash';
-<<<<<<< HEAD
-import './style.css'
-//function component() {
-  //  const element = document.createElement('div');
-    //element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    //element.classList.add('hello');
-    //return element;
-  //}
-  
-  //document.body.appendChild(component());
-  //index.html
-  
-=======
 import './style.css';
+// function component() {
+//  const element = document.createElement('div');
+// element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+// element.classList.add('hello');
+// return element;
+// }
 
-function component() {
-  const element = document.createElement('div');
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  return element;
-}
-
-document.body.appendChild(component());
->>>>>>> 708b55171cb828c6b34bc9ab45f3fa041901bd2c
+// document.body.appendChild(component());
+// index.html
